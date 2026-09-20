e93435b0da17b1e5067715763ac82c2446a217c2
LukyGive <lucaspichot999@gmail.com>
2026-09-20 22:21:49 +0700

    Delete blockbench-portfolio-v1 directory

--- blockbench-portfolio-v1/components/portfolio/FeaturedGrid.tsx
+++
@@ -1,6 +0,0 @@
-import type { Creation } from '@/lib/creations/types';
-import { CreationGallery } from './CreationGallery';
-
-export function FeaturedGrid({ creations }: { creations: Creation[] }) {
-  return <CreationGallery creations={creations} priorityCount={3} />;
-}
